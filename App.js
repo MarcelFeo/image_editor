8b2d4c42b6f35963cd24df1bb0b2e7c20b2f3149
MarcelFeo <marcelfeo29@gmail.com>
2022-05-25 11:51:03 -0300

    finalizando acesso as imagens do dispositivo

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button, Image, StyleSheet, View, TextInput } from 'react-native';
 import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
 import * as ImagePicker from 'expo-image-picker';
 
-import { Asset } from 'expo-asset';
-
 export default function App() {
   const [image, setImage] = useState(null);
-
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      setImage(result.uri);
-    }
-  };
-
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   const [originX, setOriginX] = useState(0);
@@ -31,32 +12,33 @@ export default function App() {
   const [widthResize, setWidthResize] = useState(0);
   const [heightResize, setHeightResize] = useState(0);
 
-  useEffect(() => {
-    (async () => {
-      const image = Asset.fromModule(require(`${image}`));
-      await image.downloadAsync();
-      setImage(image);
-      setReady(true);
-    })();
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      quality: 1,
+    });
 
-    pickImage();
-  }, []);
+    if (!result.cancelled) {
+      setImage(result);
+    }
+  };
 
   const rotate90andFlip = async () => {
     const manipResult = await manipulateAsync(
-      image.localUri || image,
+      image.localUri || image.uri,
       [
         { rotate: 90 },
         { flip: FlipType.Vertical },
       ],
-      { compress: 1, format: SaveFormat.JPEG || SaveFormat.PNG }
+      { compress: 1, format: SaveFormat.JPEG }
     );
     setImage(manipResult);
   };
 
   const mirrorImage = async () => {
     const manipResult = await manipulateAsync(
-      image.localUri || image,
+      image.localUri || image.uri,
       [
         { rotate: 180 },
         { flip: FlipType.Vertical },
@@ -68,7 +50,7 @@ export default function App() {
 
   const resizeImage = async () => {
     const manipResult = await manipulateAsync(
-      image.localUri || image,
+      image.localUri || image.uri,
       [
         { resize: { width: parseFloat(widthResize), height: parseFloat(heightResize) } },
       ],
@@ -79,7 +61,7 @@ export default function App() {
 
   const cropImage = async () => {
     const manipResult = await manipulateAsync(
-      image.localUri || image,
+      image.localUri || image.uri,
       [
         { crop: { height: parseFloat(height), originX: parseFloat(originX), originY: parseFloat(originY), width: parseFloat(width) } },
       ],
@@ -91,7 +73,7 @@ export default function App() {
   const renderImage = () => (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      {image && <Image source={{ uri: image.uri }} style={{ width: 300, height: 300 }} />}
     </View>
   );
 
@@ -105,15 +87,15 @@ export default function App() {
         <Button title="Mirror Image" onPress={mirrorImage} color="#ff304f" />
       </View>
       <View style={styles.buttonContainer}>
-        <TextInput style={styles.input} placeholder="Height" onChange={e => setHeight(e.target.value)} value={height || ''}></TextInput>
-        <TextInput style={styles.input} placeholder="Width" onChange={e => setWidth(e.target.value)} value={width || ''}></TextInput>
-        <TextInput style={styles.input} placeholder="originX" onChange={e => setOriginX(e.target.value)} value={originX || ''}></TextInput>
-        <TextInput style={styles.input} placeholder="originY" onChange={e => setOriginY(e.target.value)} value={originY || ''}></TextInput>
+        <TextInput style={styles.input} placeholder="Height" onChange={e => setHeight(e.target.value)} value={height || ''} keyboardType="numeric"></TextInput>
+        <TextInput style={styles.input} placeholder="Width" onChange={e => setWidth(e.target.value)} value={width || ''} keyboardType="numeric"></TextInput>
+        <TextInput style={styles.input} placeholder="originX" onChange={e => setOriginX(e.target.value)} value={originX || ''} keyboardType="numeric"></TextInput>
+        <TextInput style={styles.input} placeholder="originY" onChange={e => setOriginY(e.target.value)} value={originY || ''} keyboardType="numeric"></TextInput>
         <Button title="Crop" onPress={cropImage} color="#ff304f"/>
       </View>
       <View style={styles.buttonContainer}>
-        <TextInput style={styles.input} placeholder="Height" onChange={e => setHeightResize(e.target.value)} value={heightResize || ''}></TextInput>
-        <TextInput style={styles.input} placeholder="Width" onChange={e => setWidthResize(e.target.value)} value={widthResize || ''}></TextInput>
+        <TextInput style={styles.input} placeholder="Height" onChange={setHeightResize(value)} value={heightResize || ''} keyboardType="numeric"></TextInput>
+        <TextInput style={styles.input} placeholder="Width" onChange={setWidthResize(value)} value={widthResize || ''} keyboardType="numeric"></TextInput>
         <Button title="Resize" onPress={resizeImage} color="#ff304f" />
       </View>
     </View>
